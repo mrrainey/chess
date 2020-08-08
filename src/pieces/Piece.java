0afc4c1af948db22a15f05191218ff214da7394c
Saul Pelan <saulpelan@gmail.com>
2020-08-08 17:10:48 +0100

    Added import for Tile and made class abstract

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,12 +1,24 @@
 package pieces;
 
-public class Piece {
+import board.Tile;
+
+/**
+ * @author Saul
+ *
+ */
+public abstract class Piece {
 	private Tile currentTile;
 	
 	public Piece(Tile t) {
 		this.currentTile = t;
 	}
 	
+	
+	/**
+	 * @param t 
+	 * 
+	 * 
+	 */
 	public void move(Tile t) {
 		
 	}
