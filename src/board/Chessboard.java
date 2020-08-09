e3240c1709f706ef9d867e57432f9af1a89d87f7
Saul Pelan <saulpelan@gmail.com>
2020-08-09 23:02:46 +0100

    added some documentation

--- src/board/Chessboard.java
+++ src/board/Chessboard.java
@@ -20,8 +20,8 @@ public class Chessboard {
 				tiles[x][y] = new Tile(x, y);
 
 		for (int y = 0; y < 8; ++y) {
-			createPiece(new Pawn(p1),tiles[1][y]);
-			createPiece(new Pawn(p2),tiles[6][y]);
+			createPiece(new Pawn(p1), tiles[1][y]);
+			createPiece(new Pawn(p2), tiles[6][y]);
 		}
 
 		int[] rows = { 0, 7 };
@@ -39,11 +39,18 @@ public class Chessboard {
 
 	}
 
+	/**
+	 * Initialises the settings for a new <code>Piece</code>; linking it to its
+	 * first <code>Tile</code>.
+	 * 
+	 * @param piece the piece to initialise
+	 * @param tile  the tile to be linked
+	 */
 	private void createPiece(Piece piece, Tile tile) {
 		tile.setPiece(piece);
 		piece.setTile(tile);
 	}
-	
+
 	/**
 	 * Gets the piece at a tile using the provided coordinates.
 	 * 
@@ -54,7 +61,7 @@ public class Chessboard {
 	public Piece getPiece(int x, int y) {
 		return getTile(x, y).getPiece();
 	}
-	
+
 	/**
 	 * Gets the tile at the given coordinates.
 	 * 
@@ -89,26 +96,15 @@ public class Chessboard {
 		return false;
 	}
 
-	/**
-	 * When a <code>Pawn</code> reaches the opponent's end of the board it can be
-	 * promoted to a <code>Queen</code>, <code>Rook</code>, <code>Bishop</code> or
-	 * <code>Knight</code> as the player desires.
-	 * 
-	 * @param pawn the pawn to promote
-	 */
-	public void promote(Pawn pawn) {
-		// consult player for choice of new Piece and update the piece.
-	}
-	
 	public void printSnapshot() {
-		for (int x = 0; x < 8; ++x) 
+		for (int x = 0; x < 8; ++x)
 			for (int y = 0; y < 8; ++y) {
 				Piece p = getPiece(x, y);
 				String name = "     ";
-				if (p != null ) {
-					//System.out.println(p.getClass().getSimpleName());
+				if (p != null) {
+					// System.out.println(p.getClass().getSimpleName());
 					switch (p.getClass().getSimpleName()) {
-					
+
 					case "Bishop":
 						name = "Bshp ";
 						break;
@@ -130,10 +126,23 @@ public class Chessboard {
 
 					default:
 						break;
-					}	
+					}
 				}
-				if (y < 7) System.out.print(name + " ");
-				else System.out.println(name);
+				if (y < 7)
+					System.out.print(name + " ");
+				else
+					System.out.println(name);
 			}
+	}
+
+	/**
+	 * When a <code>Pawn</code> reaches the opponent's end of the board it can be
+	 * promoted to a <code>Queen</code>, <code>Rook</code>, <code>Bishop</code> or
+	 * <code>Knight</code> as the player desires.
+	 * 
+	 * @param pawn the pawn to promote
+	 */
+	public void promote(Pawn pawn) {
+		// consult player for choice of new Piece and update the piece.
 	}
 }
