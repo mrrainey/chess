ecfcd00cc5c98977320226c029d1dd7e83247f86
Saul Pelan <saulpelan@gmail.com>
2020-08-11 23:59:29 +0100

    added piece movement logic

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -13,9 +13,17 @@ public class Rook extends Piece {
 	}
 
 	@Override
-	public boolean canMove(Tile t) {
-		if (Navigation.isCardinal(getTile(), t)) {
-			
+	public boolean canMove(Tile target) {
+		if (Navigation.isCardinal(getTile(), target)) {
+			Tile[] path = Navigation.getPath(getTile(), target);
+			for (int i = 0; i < path.length; i++)
+				if (!path[i].isEmpty())
+					if (i < path.length)
+						return false;
+					else if (path[i].getPiece().getPlayer() == getPlayer())
+						return false;
+		} else {
+			return false;
 		}
 		return true;
 	}
