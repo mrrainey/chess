8ef4a87a287ffc7676098c9a4246a2a42e66a63a
Saul Pelan <saulpelan@gmail.com>
2020-08-15 17:03:43 +0100

    updated

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -6,7 +6,6 @@ import utilities.Navigation;
 
 public class Rook extends Piece {
 
-
 	public Rook(Player player) {
 		super(player);
 		// TODO Auto-generated constructor stub
@@ -16,17 +15,19 @@ public class Rook extends Piece {
 	public boolean canMove(Tile target) {
 		if (Navigation.isCardinal(getTile(), target)) {
 			Tile[] path = Navigation.getPath(getTile(), target);
-			for (int i = 0; i < path.length; i++)
-				if (!path[i].isEmpty())
-					if (i < path.length)
+			for (int i = 0; i < path.length; i++) {
+				if (!path[i].isEmpty()) {
+					if (i < path.length) {
 						return false;
-					else if (path[i].getPiece().getPlayer() == getPlayer())
+					} else if (path[i].getPiece().getPlayer() == getPlayer()) {
 						return false;
+					}
+				}
+			}
 		} else {
 			return false;
 		}
 		return true;
 	}
-	
-	
+
 }
