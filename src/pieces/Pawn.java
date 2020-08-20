baafc9c9a08e96bbcc102ffdf211f6e83123d259
Saul Pelan <saulpelan@gmail.com>
2020-08-20 22:41:18 +0100

    added movement logic

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -1,7 +1,10 @@
 package pieces;
 
 import board.Tile;
+import chess.Chess;
 import player.Player;
+import utilities.Navigation;
+import utilities.Navigation.Direction;
 
 public class Pawn extends Piece {
 
@@ -10,10 +13,30 @@ public class Pawn extends Piece {
 	}
 
 	@Override
-	public boolean canMove(Tile t) {
-		// pawn movement logic
-		return true;
+	public boolean canMove(Tile target) {
+		Navigation.Direction dir = Navigation.getDirection(getTile(), target);
+		int dist = Navigation.getDistance(getTile(), target);
+		if (dir == (getPlayer() == Chess.getBoard().getPlayer1() ? Direction.South : Direction.North)) {
+			if (target.isEmpty()) {
+				if (dist == 1) {
+					return true;
+				} else if (dist == 2) {
+					if (getMoves() == 0) {
+						return true;
+					}
+				}
+			}
+		} else if (dir == (getPlayer() == Chess.getBoard().getPlayer1() ? Direction.SouthEast : Direction.NorthEast)
+				|| dir == (getPlayer() == Chess.getBoard().getPlayer1() ? Direction.SouthWest : Direction.NorthWest)) {
+			if (dist == 1) {
+				if (!target.isEmpty()) {
+					if (target.getPiece().getPlayer() != getPlayer()) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
-	
-	
+
 }
