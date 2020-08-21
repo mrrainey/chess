4ec6bf5cf081759022feb52d81de908f5f911553
Saul Pelan <saulpelan@gmail.com>
2020-08-21 19:00:56 +0100

    added movement logic

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -2,19 +2,27 @@ package pieces;
 
 import board.Tile;
 import player.Player;
+import utilities.Navigation;
 
 public class Knight extends Piece {
-
 
 	public Knight(Player player) {
 		super(player);
 	}
 
 	@Override
-	public boolean canMove(Tile t) {
-		// movement logic
-		return false;
+	public boolean canMove(Tile target) {
+		final Tile[] path = Navigation.getKnightPath(getTile(), target);
+		if (path != null) {
+			if (!path[path.length - 1].isEmpty()) {
+				if (path[path.length - 1].getPiece().getPlayer() == getPlayer()) {
+					return false;
+				}
+			}
+		} else {
+			return false;
+		}
+		return true;
 	}
-	
-	
+
 }
